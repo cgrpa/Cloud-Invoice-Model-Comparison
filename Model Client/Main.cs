d58ecd37d3eba14c3c31b94c6d7df96cd5f49c05
Che Gamble <chegamblerpa@gmail.com>
2022-12-17 13:12:31 +0000

    add main project files

---
+++ Model Client/Main.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Invoice_Model_Comparison.Model_Client
+{
+    internal class Main
+    {
+        static void Main()
+        {
+
+        }
+    }
+}
